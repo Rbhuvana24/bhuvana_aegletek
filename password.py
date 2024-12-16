db8f7a2d0bb583880da466c7e2230dfac4084eca
bhuvana <bhuvaneswari.rajavarapu@aegletek.com>
2024-12-16 10:51:13 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,15 @@
+
+
+
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choices(characters, k=length))
+    return password
+
+
+password = generate_password(34)
+print(f"Generated password: {password}")
+
